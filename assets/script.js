09cd9e421b01813c8f51d28cf888124381da98cb
Corbin Ball <c.ball@me.com>
2021-09-10 18:04:03 -0500

    added js file from my rough draft

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+//to display current date and time at the top
+var currentTime = $('#currentDay');
+
+var timeEl= $('<h2>');
+
+timeEl.attr('hour');
+timeEl = moment().format('MMMM Do YYYY, h:mm a');
+eventTracker = moment().format('h a');
+console.log(eventTracker);
+console.log(timeEl);
+
+currentTime.append(timeEl);
+
+
+
+
+
+var eventSubmit = $('#eventInput');
+var eventLog = $('#events-listed');
+
+function addEvent(event) {
+    event.preventDefault();
+
+    var eventAdd = $('input[name="event-entry"]').val();
+
+    if (!eventAdd) {
+        console.log('No event added');
+        return;
+    }
+
+    eventLog.append(eventAdd);
+    console.log(eventLog);
+    console.log(eventAdd);
+
+    $('input[name="event-entry"]').val('');
+
+}
+
+eventSubmit.on('submit', addEvent, eventTime);
+
+eventSubmit.append('#eventInput');
+
+function eventTime() {
+
+    if (timeEl === eventTracker) {
+        eventEl.addClass('present')
+    }
+
+    if (timeEl < eventTracker) {
+        eventEl.addClass('past')
+    }
+
+    if (timeEl > eventTracker) {
+        eventEl.addClass('future')
+    }
+}
+
